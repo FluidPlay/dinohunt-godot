90c77548a0ccc51e2a340099e0ed91dadbbd9e31
Breno Azevedo <maddox.br@gmail.com>
2024-11-27 11:51:40 -0300

    * Fixed scene-loading bug

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,14 +4,14 @@ signal on_set_health
 signal on_add_score
 signal on_start_game
 
-@export var P1Health_label:RichTextLabel
-@export var P2Health_label:RichTextLabel
+@export var P1Health_label: RichTextLabel
+@export var P2Health_label: RichTextLabel
 
-@export var P1Score_label:RichTextLabel
-@export var P2Score_label:RichTextLabel
+@export var P1Score_label: RichTextLabel
+@export var P2Score_label: RichTextLabel
 
-@export var intro_scene : PackedScene
-@export var game_scene : PackedScene
+@export var intro_scene_name: String = "intro"
+@export var game_scene_name: String = "game"
 
 enum reload_scene_options { intro_scene, game_scene }
 @export var reload_scene_on_death: reload_scene_options
@@ -30,8 +30,9 @@ func _ready() -> void:
 	instance = self
 	
 func start_game() -> void:
-	if !game_scene == null:
-		load_scene.call_deferred(game_scene)
+	if game_scene_name != "":
+		#load_scene.call_deferred(game_scene)
+		load_scene(game_scene_name)
 	
 func set_health(newHealth, pNumber):
 	if (pNumber == 0):
@@ -44,15 +45,16 @@ func death():
 	get_tree().paused = true
 	await get_tree().create_timer(reload_scene_delay).timeout
 	print("Load next")
-	var next_scene = intro_scene if reload_scene_on_death == reload_scene_options.intro_scene \
-								 else game_scene
+	var next_scene = intro_scene_name \
+					 if reload_scene_on_death == reload_scene_options.intro_scene \
+						else game_scene_name
 
 	#load_scene.call_deferred(next_scene)
 	load_scene(next_scene)
 	get_tree().paused = false
 	
 func load_scene(next_scene):
-	SceneSwitcher.switch_scene(next_scene.resource_path)
+	SceneSwitcher.switch_scene("res://GameScenes/"+next_scene+".tscn")
 		
 func add_score(points, pNumber):
 	print("p number: "+str(pNumber))
--- Scripts/LoadScene.gd
+++ Scripts/LoadScene.gd
@@ -1,11 +1,12 @@
 extends Node
 
 signal on_load_scene
-@export var scene_to_load: PackedScene
+#@export var scene_to_load: PackedScene
+@export var scene_name: String = "game"
 
 func _ready() -> void:
 	on_load_scene.connect(load_scene)
 
 func load_scene() -> void:
-	if !scene_to_load == null:
-		SceneSwitcher.switch_scene(scene_to_load.resource_path)
+	if !scene_name == "":
+		SceneSwitcher.switch_scene("res://GameScenes/"+scene_name+".tscn")
